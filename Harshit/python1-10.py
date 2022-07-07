ddb53a65618fcbe39e9f6a8ff340dacc322ccb92
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2022-07-07 13:43:52 -0700

    Add files via upload

---
+++ Harshit/python1-10.py
@@ -0,0 +1,89 @@
+#####################CODE1#######################
+# print('''Twinkle, twinkle, little star,
+# 	How I wonder what you are! 
+# 		Up above the world so high,   		
+# 		Like a diamond in the sky. 
+# Twinkle, twinkle, little star, 
+# 	How I wonder what you are''')
+
+
+#####################CODE2#######################
+# import sys
+# print("Python version","Version info.",sys.version_info)
+
+
+#####################CODE 3#######################
+# Write a Python program to display the current date and time.
+# # first we have to import the date and time moodule
+# import datetime
+
+# #now we are calling the date and time function fromthe date time module 
+# now = datetime.datetime.now()
+
+# # printing the current date 
+# print ("Current date and time : ")
+
+# # now we aregiving the format of the print
+# print (now)
+
+
+#####################CODE 4#######################
+
+# where r is a veriable 
+# R = float(input("radius of circle "))
+
+# # now creating a function
+
+# def myfun():
+#     print(R*R*22/7)
+# # ""we do not put as it will count it as veriable
+# myfun()
+
+
+#####################CODE 5#######################
+# X = (input("first name "))
+
+# y = (input("lastname "))
+
+ # print(y, X)
+
+
+#####################CODE6#######################
+# . Write a Python program which accepts a sequence of comma-separated numbers from user and 
+# # generate a list and a tuple with those numbers
+
+
+# a = input("input some comma seprated number :")
+# b = a.split(",")
+# print('list:',b)
+# c = tuple(b) #tuple is a function here hece we cannot change
+# print('Tuple :',c)
+
+#####################CODE7#######################
+# x = input ("name of file")
+# b = x.split(".")
+# print(b[1])
+
+
+#####################CODE 8#######################
+# color_list = ["red","green","white","black"]
+# # this is call indexing 
+# print(color_list[0],color_list[-1])
+
+
+# # the index start from the zero in the list and we can use -1 to start from the end 
+
+
+#####################CODE9#######################
+# exam_st_date = (11,12,2014)
+
+# print( "The examination will start from : %i / %i / %i"%exam_st_date)
+
+
+
+#####################CODE10#######################
+# Write a Python program to display the examination schedule. (extract the date from exam_st_date).
+# exam_st_date = (14,7,2022)
+# print( "The examination will start from : %i / %i / %i"%exam_st_date)
+
+
