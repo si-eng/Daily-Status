28ccb2617d24a32b02c70bb05146e4e56710d98f
priyashukla1411 <100112297+priyashukla1411@users.noreply.github.com>
2022-07-08 17:40:20 +0530

    Update assignment.py

--- Priya/assignment.py
+++ Priya/assignment.py
@@ -409,6 +409,11 @@
 #     print('*', end="")
 # print("\n")
 
+# 51. Write a Python program to determine profiling of Python programs
+# import cProfile                 #(Python includes a built in module called cProfile which is used to measure the execution time of a program.)
+# cProfile.run("10 + 10")
+
+
 # 53. Write a python program to access environment variables.
 # import os
 # # Access all environment variables 
@@ -429,8 +434,12 @@
 # p=os.getlogin()
 # print(p)
 
-# 58. Write a Python program to sum of the first n positive integers
+# 56. Write a Python program to get height and width of the console window
+# import os
+# size = os.get_terminal_size()
+# print(size)
 
+# 58. Write a Python program to sum of the first n positive integers
 # n = int(input("Input a number: "))
 # sum_num = (n * (n + 1)) / 2
 # print("Sum of the first", n ,"positive integers:", sum_num)
@@ -557,11 +566,46 @@
 # print( "The midpoint's y value is: ",y_m_point)
 # print();
 
+#74. Write a Python program to hash a word
+# hash for integer unchanged
+#print('Hash for 181 is:', hash(1851.4))
+# hash for decimal
+#print('Hash for 181.23 is:',hash(181.23))
+# hash for string
+#print('Hash for Python is:', hash('Python'))
+
 # 75. Write a Python program to get the copyright information and write Copyright information in Python code.
 # import sys
 # print("\nPython Copyright Information")
 # print(sys.copyright)
 # print()
+
+#78. Write a Python program to find the available built-in modules.
+# import sys
+# import textwrap
+# module_name = ', '.join(sorted(sys.builtin_module_names))
+# print(textwrap.fill(module_name, width=20))
+
+# 79. Write a Python program to get the size of an object in bytes.
+# import sys
+# str1 = "one"
+# str2 = "two"
+# str3 = "three"
+# x = 0
+# y = 112
+# print("Size of ",str1,"=",str(sys.getsizeof(str1))+ " bytes")
+# print("Size of ",str2,"=",str(sys.getsizeof(str2))+ " bytes")
+# print("Size of ",str3,"=",str(sys.getsizeof(str3))+ " bytes")
+# print("Size of",x,"=",str(sys.getsizeof(x))+ " bytes")
+# print("Size of" ,y,"="+str(sys.getsizeof(y))+ " bytes")
+# L = [1, 2, 3, 'Red', 'Black']
+# print("Size of",L,"=",sys.getsizeof(L)," bytes")
+# T = ("Red", [8, 4, 6], (1, 2, 3))
+# print("Size of",T,"=",sys.getsizeof(T)," bytes")
+# S = {'apple', 'orange', 'apple', 'pear'}
+# print("Size of",S,"=",sys.getsizeof(S)," bytes")
+# D = {'Name': 'priya', 'Age': 21}
+# print("Size of",D,"=",sys.getsizeof(S)," bytes")
 
 # 80. Write a Python program to get the current value of the recursion limit.
 # import sys
@@ -634,6 +678,10 @@
 # if n == 1:
 #    print("\nFirst day of a Month!")
 # print()
+
+
+# 90. Write a Python program to create a copy of its own source code. 
+# s='s=%r;print(s%%s'; print(s%s)
 
 # 91. Write a Python program to swap two variables.
 # a = 30
