0da2d218f76d64914a4fa798a43146505661290b
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2022-07-09 06:06:45 -0700

    Update Q (1).py

--- Shubham/Assignment-01/Q (1).py
+++ Shubham/Assignment-01/Q (1).py
@@ -1,3 +1,1 @@
-print(abs.__doc__)
-x = abs(-67.78)
-print(x)
+print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!")
