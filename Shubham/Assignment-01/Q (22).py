b5ba9525af44c63bb1bb717ed072bf4c3b97a0ac
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2022-07-09 23:28:16 +0530

    Update Q (22).py

--- Shubham/Assignment-01/Q (22).py
+++ Shubham/Assignment-01/Q (22).py
@@ -1,3 +1,7 @@
-print(abs.__doc__)
-x = abs(-67.78)
-print(x)
+def myfunc(num):
+     count=0
+     for i in num:
+         if i == 4:
+             count=count+1
+     return count
+ print(myfunc([1,4,2,3,5,6,4,4]))
