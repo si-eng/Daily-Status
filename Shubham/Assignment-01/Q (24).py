e07d230adbecbb12631ba56a39822a880938ab27
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2022-07-09 23:29:46 +0530

    Update Q (24).py

--- Shubham/Assignment-01/Q (24).py
+++ Shubham/Assignment-01/Q (24).py
@@ -1,3 +1,6 @@
-print(abs.__doc__)
-x = abs(-67.78)
-print(x)
+x= input("Enter value for x :")
+y= input("Enter value for y :")
+z= input("Enter value for z :")
+
+if (x==y and y==z):
+    print("\nAll entered values for x y and z are Equal.")
