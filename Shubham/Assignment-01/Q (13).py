536e1b12a823ac2270bb8d1ad183a1823262e1b5
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2022-07-09 06:14:31 -0700

    Update Q (13).py

--- Shubham/Assignment-01/Q (13).py
+++ Shubham/Assignment-01/Q (13).py
@@ -3,3 +3,11 @@ a string that you "don't" have to escape
 This
 is a ....... multi-line
 heredoc string --------> example
+
+
+# print("""
+# Sample string :
+# a string that you "don't" have to escape
+# This
+# is a ....... multi-line
+# heredoc string --------> example""")
