565f9497d1e2cd08a73762cde528641363b23c41
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2022-07-09 07:46:51 -0700

    Update Q (17).py

--- Shubham/Assignment-01/Q (17).py
+++ Shubham/Assignment-01/Q (17).py
@@ -1,3 +1,6 @@
-print(abs.__doc__)
-x = abs(-67.78)
-print(x)
+def near_thousand(n):
+  return ((abs(1000 - n) <= 100) or (abs(2000 - n) <= 100))
+print(near_thousand(1000))
+print(near_thousand(900))
+print(near_thousand(800))   
+print(near_thousand(2200))
