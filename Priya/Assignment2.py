01c6437689fc6cc296702765bfc9f370f49aeb26
shreechandra <100112433+shreechandra@users.noreply.github.com>
2022-07-20 18:08:44 +0530

    Update Assignment2.py

--- Priya/Assignment2.py
+++ Priya/Assignment2.py
@@ -5,7 +5,7 @@
 #       else:
 #         return False;
 # print(test_distinct([1,5,7]))
-# print(test_distinct([2,4,5,5,7,9]))
+# print(test_distinct([2,4,5,5,7]))
 
 #2. Write a Python program to create all possible strings by using 'a', 'e', 'i', 'o', 'u'. Use the characters exactly once.
 # import random
