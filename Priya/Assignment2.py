316565347cd9942117c8ebd49caf9f2eb1892a98
priyashukla1411 <100112297+priyashukla1411@users.noreply.github.com>
2022-07-22 18:06:22 +0530

    Update Assignment2.py

--- Priya/Assignment2.py
+++ Priya/Assignment2.py
@@ -270,3 +270,15 @@
 #     cord_data.append(x)
 #     cord_data.append(y)
 # print("\nArea of the Polygon:",poly_area(cord_data))
+
+#60. Internet search engine giant, such as Google accepts web pages around the world and classify them, creating a huge database. The search engines also analyze the search keywords entered by the user and create inquiries for database search. In both cases, complicated processing is carried out in order to realize efficient retrieval, but basics are all cutting out words from sentences.
+
+#Write a Python program to cut out words of 3 to 6 characters length from a given sentence not more than 1024 characters.
+# print("Input a sentence (1024 characters. max.)")
+# yy = input()
+# yy = yy.replace(",", " ")
+# yy = yy.replace(".", " ")
+# print("3 to 6 characters length of words:")
+# print(*[y for y in yy.split() if 3 <= len(y) <= 6])
+
+
