85a0ac989166a3fbd8c3944f8c680279539980fd
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2022-07-23 22:48:21 +0530

    Update Q (31).py

--- Shubham/Assignment-01/Q (31).py
+++ Shubham/Assignment-01/Q (31).py
@@ -1,3 +1,14 @@
-print(abs.__doc__)
-x = abs(-67.78)
-print(x)
+x = int(input('Enter first digit : '))
+y = int(input('Enter second digit : '))
+
+def gcd(x, y):
+   gcd = 1   
+   if x % y == 0:
+       return y   
+   for k in range(int(y / 2), 0, -1):
+       if x % k == 0 and y % k == 0:
+           gcd = k
+           break 
+   return gcd
+
+print("GCD of numbers are : ", gcd(x,y))
