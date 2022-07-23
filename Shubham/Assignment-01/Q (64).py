c6078feec1f9d7740372a2a447844d4b28c43bb4
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2022-07-23 23:32:22 +0530

    Update Q (64).py

--- Shubham/Assignment-01/Q (64).py
+++ Shubham/Assignment-01/Q (64).py
@@ -1,3 +1,4 @@
-print(abs.__doc__)
-x = abs(-67.78)
-print(x)
+import os.path, time
+filename = input("ENter file Name: ")
+print("Last modified:" , time.ctime(os.path.getmtime(filename)))
+print("Created: ", time.ctime(os.path.getctime(filename)))
