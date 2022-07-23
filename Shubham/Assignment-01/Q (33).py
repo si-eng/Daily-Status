a01c92e79495c36eeaa1373e088bf97f18b5028b
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2022-07-23 22:49:39 +0530

    Update Q (33).py

--- Shubham/Assignment-01/Q (33).py
+++ Shubham/Assignment-01/Q (33).py
@@ -1,3 +1,9 @@
-print(abs.__doc__)
-x = abs(-67.78)
-print(x)
+x = int(input('Enter first digit : '))
+y = int(input('Enter second digit : '))
+z = int(input('Enter third digit : '))
+
+
+if (x==y or y==z or z==x ):
+    print("sum is 0")
+else:
+    print("sum is ", x+y+z)
