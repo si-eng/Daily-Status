4356e710a4167971b96e2f77bec27fe47eb872f1
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2022-07-23 23:30:33 +0530

    Update Q (61).py

--- Shubham/Assignment-01/Q (61).py
+++ Shubham/Assignment-01/Q (61).py
@@ -1,3 +1,8 @@
-print(abs.__doc__)
-x = abs(-67.78)
-print(x)
+d_ft = int(input("Input distance in feet: "))
+d_inches = d_ft * 12
+d_yards = d_ft / 3.0
+d_miles = d_ft / 5280.0
+
+print("The distance in inches is: " , d_inches)
+print("The distance in yards is : " , d_yards)
+print("The distance in miles is : ", d_miles)
