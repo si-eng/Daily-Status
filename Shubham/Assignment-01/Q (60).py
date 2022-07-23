204a1504b3b7fc47bc4cc3be9ecbe585cc570d27
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2022-07-23 23:29:51 +0530

    Update Q (60).py

--- Shubham/Assignment-01/Q (60).py
+++ Shubham/Assignment-01/Q (60).py
@@ -1,3 +1,8 @@
-print(abs.__doc__)
-x = abs(-67.78)
-print(x)
+from math import sqrt
+
+b = float(input("Enter base length : "))
+p = float(input("Enter perpendcular length : "))
+
+h = sqrt( b**2 + p**2 )
+
+print("hypotenuse of a right angled triangle is: ", h)
