ac368e03aef314f7050fc5cf0bb7c828b8c281a6
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2022-07-23 23:33:06 +0530

    Update Q (65).py

--- Shubham/Assignment-01/Q (65).py
+++ Shubham/Assignment-01/Q (65).py
@@ -1,3 +1,9 @@
-print(abs.__doc__)
-x = abs(-67.78)
-print(x)
+time = float(input("Input time in seconds: "))
+day = time // (24 * 3600)
+time = time % (24 * 3600)
+hour = time // 3600
+time %= 3600
+minutes = time // 60
+time %= 60
+seconds = time
+print(f"day : {day} , hour : {hour} , minute : {minutes}, seconds : {seconds} ")
