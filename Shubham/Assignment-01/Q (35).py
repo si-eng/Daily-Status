f1d79de233ec94d96511ca7d9feeb67c1db7ab77
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2022-07-23 22:51:21 +0530

    Update Q (35).py

--- Shubham/Assignment-01/Q (35).py
+++ Shubham/Assignment-01/Q (35).py
@@ -1,3 +1,7 @@
-print(abs.__doc__)
-x = abs(-67.78)
-print(x)
+x = int(input('Enter first digit : '))
+y = int(input('Enter second digit : '))
+
+sum = x+y
+
+if (x==y or sum == 5):
+    print("True")
