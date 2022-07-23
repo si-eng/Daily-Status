1ae1ef5b24c8e7fbfaa3d3fd95344555ff783f67
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2022-07-23 22:50:22 +0530

    Update Q (34).py

--- Shubham/Assignment-01/Q (34).py
+++ Shubham/Assignment-01/Q (34).py
@@ -1,3 +1,9 @@
-print(abs.__doc__)
-x = abs(-67.78)
-print(x)
+x = int(input('Enter first digit : '))
+y = int(input('Enter second digit : '))
+
+sum = x+y
+
+if (sum >=15 and sum <=20):
+    print("sum is 20")
+else:
+    print("sum is ", sum )
