42462a42723e977db36010fee700bda7cb2832a6
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2022-07-23 22:59:43 +0530

    Update Q (43).py

--- Shubham/Assignment-01/Q (43).py
+++ Shubham/Assignment-01/Q (43).py
@@ -1,3 +1,5 @@
-print(abs.__doc__)
-x = abs(-67.78)
-print(x)
+import platform
+import os
+print("Name of the operating system:",os.name)
+print("\nName of the OS system:",platform.system())
+print("\nVersion of the operating system:",platform.release())
