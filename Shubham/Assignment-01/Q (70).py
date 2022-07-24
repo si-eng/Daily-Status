66027c3ab30143a1b4a450e8aa0c07c967939753
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2022-07-23 23:40:12 -0700

    Update Q (70).py

--- Shubham/Assignment-01/Q (70).py
+++ Shubham/Assignment-01/Q (70).py
@@ -1,3 +1,7 @@
-print(abs.__doc__)
-x = abs(-67.78)
-print(x)
+import glob
+from ntpath import join
+import os
+
+files = glob.glob("*.py")
+files.sort(key=os.path.getmtime)
+print("\n".join(files))
