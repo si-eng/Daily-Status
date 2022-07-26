0a074509ec353a54784e8364efb6cf89b1a2ecd6
Vikrant1020 <98014253+Vikrant1020@users.noreply.github.com>
2022-07-26 06:06:32 +0530

    Add files via upload

---
+++ Harshit/python71-80.py
@@ -0,0 +1,70 @@
+############Q71############
+
+############Q72############
+# # Imports the math module
+# import math            
+# #Sets everything to a list of math module
+# math_ls = dir(math) # 
+# print(math_ls)
+############Q73############
+# print('\nCalculate the midpoint of a line :')
+
+# x1 = float(input('The value of x (the first endpoint) '))
+# y1 = float(input('The value of y (the first endpoint) '))
+
+# x2 = float(input('The value of x (the first endpoint) '))
+# y2 = float(input('The value of y (the first endpoint) '))
+
+# x_m_point = (x1 + x2)/2
+# y_m_point = (y1 + y2)/2
+# print();
+# print("The midpoint of line is :")
+# print( "The midpoint's x value is: ",x_m_point)
+# print( "The midpoint's y value is: ",y_m_point)
+# print();
+############Q74############
+# hash for integer unchanged
+#print('Hash for 181 is:', hash(1851.4))
+# hash for decimal
+#print('Hash for 181.23 is:',hash(181.23))
+# hash for string
+#print('Hash for Python is:', hash('Python')
+############Q75############
+# import sys
+# print("\nPython Copyright Information")
+# print(sys.copyright)
+# print()
+############Q76############
+# import sys
+# import textwrap
+# module_name = ', '.join(sorted(sys.builtin_module_names))
+# print(textwrap.fill(module_name, width=20))
+############Q79############
+# import sys
+# str1 = "one"
+# str2 = "two"
+# str3 = "three"
+# x = 0
+# y = 112
+# print("Size of ",str1,"=",str(sys.getsizeof(str1))+ " bytes")
+# print("Size of ",str2,"=",str(sys.getsizeof(str2))+ " bytes")
+# print("Size of ",str3,"=",str(sys.getsizeof(str3))+ " bytes")
+# print("Size of",x,"=",str(sys.getsizeof(x))+ " bytes")
+# print("Size of" ,y,"="+str(sys.getsizeof(y))+ " bytes")
+# L = [1, 2, 3, 'Red', 'Black']
+# print("Size of",L,"=",sys.getsizeof(L)," bytes")
+# T = ("Red", [8, 4, 6], (1, 2, 3))
+# print("Size of",T,"=",sys.getsizeof(T)," bytes")
+# S = {'apple', 'orange', 'apple', 'pear'}
+# print("Size of",S,"=",sys.getsizeof(S)," bytes")
+# D = {'Name': 'priya', 'Age': 21}
+# print("Size of",D,"=",sys.getsizeof(S)," bytes")
+############Q71############
+############Q71############
+############Q71############
+############Q80############
+# import sys
+# print()
+# print("Current value of the recursion limit:")
+# print(sys.getrecursionlimit())
+# print()
---
+++ Harshit/python81-90.py
@@ -0,0 +1,67 @@
+############Q81############
+# list_of_colors = ['Red', 'White', 'Black']  
+# colors = '-'.join(list_of_colors)
+# print()
+# print("All Colors: ",colors)
+# print()
+############Q82############
+# s = sum([10,20,30])
+# print("\nSum of the container: ", s)
+# print()
+
+############Q83############
+# num = [2, 3, 4, 5]
+# print()
+# print(all(x > 1 for x in num))
+# print(all(x > 4 for x in num))
+# print()
+############Q84############
+# s = "The quick brown fox jumps over the lazy dog."  
+# print("Original string:")
+# print(s)
+# print("Number of occurrence of 'o' in the said string:")
+# print(s.count("o"))
+############Q85############
+# import os  
+# path="question1.py"  
+# if os.path.isdir(path):  
+#     print("It is a directory")  
+# elif os.path.isfile(path):  
+#     print("It is a normal file")  
+# else:  
+#     print("It is a special file (socket, FIFO, device file)" )
+# print()
+############Q86############
+# print(ord('a'))                     
+# print(ord('A'))
+# print(ord('1'))
+# print(ord('@'))
+
+############Q87############
+#87. Write a Python program to get the size of a file.
+# import os
+# file_size = os.path.getsize("question1.py")
+# print("The size of abc.txt is :",file_size,"Bytes")
+# print()
+############Q88############
+# x = 30
+# y = 20
+# print("\n%d+%d=%d" % (x, y, x+y))
+# print()
+############Q89############
+# Given a variable name, if the value is 1, display the string "First day of a Month!" and do nothing if the value is not equal.
+# n=1
+# if n == 1:
+#    print("\nFirst day of a Month!")
+# print(
+############Q90############
+# s='s=%r;print(s%%s'; print(s%s)
+
+# 91. Write a Python program to swap two variables.
+# a = 30
+# b = 20
+# print("\nBefore swap a = %d and b = %d" %(a, b))
+# a, b = b, a
+# print("\nAfter swaping a = %d and b = %d" %(a, b))
+# print(
+#
