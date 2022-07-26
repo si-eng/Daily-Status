ba618f248eb63d66b8a0a37c3ca45ee00e7485a1
priyashukla1411 <100112297+priyashukla1411@users.noreply.github.com>
2022-07-26 17:49:54 +0530

    Update Assignment2.py

--- Priya/Assignment2.py
+++ Priya/Assignment2.py
@@ -281,4 +281,96 @@
 # print("3 to 6 characters length of words:")
 # print(*[y for y in yy.split() if 3 <= len(y) <= 6])
 
+#61. Arrange integers (0 to 99) as narrow hilltop, as illustrated in Figure 1. Reading such data representing huge, when starting from the top and proceeding according to the next rule to the bottom.
+# Write a Python program that compute the maximum value of the sum of the passing integers.
 
+'''import sys
+print("Input the numbers (ctrl+d to exit):")
+nums = [list(map(int, l.split(","))) for l in sys.stdin]
+mvv = nums[0]
+for i in range(1, (len(nums)+1)//2):
+    rvv = [0]*(i+1)
+    for j in range(i):
+        rvv[j] = max(rvv[j], mvv[j]+nums[i][j])
+        rvv[j+1] = max(rvv[j+1], mvv[j]+nums[i][j+1])
+    mvv = rvv
+for i in range((len(nums)+1)//2, len(nums)):
+    rvv = [0]*(len(mvv)-1)
+    for j in range(len(rvv)):
+        rvv[j] = max(mvv[j], mvv[j+1]) + nums[i][j]
+    mvv = rvv
+print("Maximum value of the sum of integers passing according to the rule on one line.") 
+print(mvv[0])'''
+
+
+#62. Write a Python program to find the number of combinations that satisfy p + q + r + s = n 
+# where n is a given number <= 4000 and p, q, r, s in the range of 0 to 1000.
+'''from collections import Counter
+print("Input a positive integer: (ctrl+d to exit)") 
+pair_dict = Counter()
+for i in range(2001):
+  pair_dict[i] = min(i, 2000 - i) + 1 
+while True:
+  try:
+    n = int(input())
+    ans = 0
+    for i in range(n + 1):
+      ans += pair_dict[i] * pair_dict[n - i]
+    print("Number of combinations of a,b,c,d:",ans) 
+  except EOFError:
+    break'''
+    
+#63. Write a Python program which adds up columns and rows of given table as shown in the specified figure. Go to the editor
+#Input number of rows/columns (0 to exit)
+#4
+#Input cell value:
+# 25 69 51 26
+# 68 35 29 54
+# 54 57 45 63
+# 61 68 47 59
+# Result:
+# 25 69 51 26 171
+# 68 35 29 54 186
+# 54 57 45 63 219
+# 61 68 47 59 235
+# 208 229 172 202 811
+# Input number of rows/columns (0 to exit)
+'''while True:
+    print("Input number of rows/columns (0 to exit)")
+    n = int(input())
+    if n == 0:
+        break
+    print("Input cell value:")
+    x = []
+    for i in range(n):
+        x.append([int(num) for num in input().split()])
+
+    for i in range(n):
+        sum = 0
+        for j in range(n):
+            sum += x[i][j]
+        x[i].append(sum)
+
+    x.append([])
+    for i in range(n + 1):
+        sum = 0
+        for j in range(n):
+            sum += x[j][i]
+        x[n].append(sum)
+    print("Result:")
+    for i in range(n + 1):
+        for j in range(n + 1):
+            print('{0:>5}'.format(x[i][j]), end="")
+        print()'''
+    
+#64. Given a list of numbers and a number k, write a Python program to check whether the sum of any two numbers from the list is equal to k or not.ll    
+'''def check_sum(nums, k):   
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == k:
+                return True
+    return False
+print(check_sum([12, 5, 0, 5], 10))
+print(check_sum([20, 20, 4, 5], 40))
+print(check_sum([1, -1], 0))
+print(check_sum([1, 1, 0], 0)) 
