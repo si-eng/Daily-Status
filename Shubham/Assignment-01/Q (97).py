ef61321b50e3fa833834f656db3c5208d8bcf8eb
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2022-07-27 22:28:30 +0530

    Update Q (97).py

--- Shubham/Assignment-01/Q (97).py
+++ Shubham/Assignment-01/Q (97).py
@@ -1,3 +1,4 @@
-print(abs.__doc__)
-x = abs(-67.78)
-print(x)
+s_var_names = sorted((set(globals().keys()) | set(__builtins__.__dict__.keys())) - set('_ names i'.split()))
+print()
+print(s_var_names)
+# print( '\n'.join(' '.join(s_var_names[i:i+8]) for i in range(0, len(s_var_names), 8)) )
