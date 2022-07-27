7ca6851ccf7600ef8ae94f0978a1990bb08b04ab
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2022-07-27 22:33:24 +0530

    Update Q (107).py

--- Shubham/Assignment-01/Q (107).py
+++ Shubham/Assignment-01/Q (107).py
@@ -1,3 +1,8 @@
-print(abs.__doc__)
-x = abs(-67.78)
-print(x)
+import os.path
+import time
+
+print('File         :', __file__)
+print('Access time  :', time.ctime(os.path.getatime(__file__)))
+print('Modified time:', time.ctime(os.path.getmtime(__file__)))
+print('Change time  :', time.ctime(os.path.getctime(__file__)))
+print('Size         :', os.path.getsize(__file__))
