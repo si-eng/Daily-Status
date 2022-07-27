9108ea01295857534ee5afae2b1ae14c2e2c80be
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2022-07-27 22:16:24 +0530

    Update Q (73).py

--- Shubham/Assignment-01/Q (73).py
+++ Shubham/Assignment-01/Q (73).py
@@ -1,3 +1,10 @@
-print(abs.__doc__)
-x = abs(-67.78)
-print(x)
+x1 = float(input("Enter X cordinate of first pofloat : "))
+y1 = float(input("Enter Y cordinate of first pofloat : "))
+
+x2 = float(input("Enter X cordinate of second pofloat : "))
+y2 = float(input("Enter Y cordinate of second pofloat : "))
+
+x3 = (x1+x2)/2
+y3 = (y1+y2)/2
+
+print("Mid pofloat is ", x3,y3)
