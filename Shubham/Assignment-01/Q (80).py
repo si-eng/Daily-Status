b07f378c96e5c534ad6142415503d19f960f67ba
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2022-07-27 22:20:02 +0530

    Update Q (80).py

--- Shubham/Assignment-01/Q (80).py
+++ Shubham/Assignment-01/Q (80).py
@@ -1,3 +1,9 @@
-print(abs.__doc__)
-x = abs(-67.78)
-print(x)
+import sys
+print("Current value of the recursion limit:")
+print(sys.getrecursionlimit())
+
+######################################################
+# Python's default recursion limit is 1000, meaning that Python won't let a function call on itself more than 1000 times, 
+# which for most people is probably enough. It can be set by setrecursionlimit().
+# The limit exists because allowing recursion to occur more than 1000 times doesn't exactly make for lightweight code.
+###################################################################
