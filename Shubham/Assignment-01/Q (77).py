ccc687bc2b794071ffc44d04dcd9fd820b28177a
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2022-07-27 22:18:13 +0530

    Update Q (77).py

--- Shubham/Assignment-01/Q (77).py
+++ Shubham/Assignment-01/Q (77).py
@@ -1,3 +1,9 @@
-print(abs.__doc__)
-x = abs(-67.78)
-print(x)
+import sys
+print()
+if sys.byteorder == "little":
+    #intel, alpha
+    print("Little-endian platform.")
+else:
+    #motorola, sparc
+    print("Big-endian platform.")
+print()
