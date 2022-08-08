147c564e5db87347b1b365b4d8fc2fe741e99428
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2022-08-08 10:16:00 +0530

    Update Loadbalancer.tf

--- Shubham/Assignment 03/Loadbalancer.tf
+++ Shubham/Assignment 03/Loadbalancer.tf
@@ -17,3 +17,14 @@ resource "aws_lb" "test" {
     Name = "Mylb"
   }
 }
+
+resource "aws_lb_listener" "LG" {
+  load_balancer_arn = aws_lb.test.arn
+  port              = "80"
+#   protocol          = "Custom"
+  
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.TG.arn
+  }
+}
