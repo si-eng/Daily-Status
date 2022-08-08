b6b20e0f188d83b539515a402a71797b981e1fb4
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2022-08-08 10:14:54 +0530

    Update SNS.tf

--- Shubham/Assignment 03/SNS.tf
+++ Shubham/Assignment 03/SNS.tf
@@ -7,3 +7,20 @@ resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
   protocol  = "email"
   endpoint  = "vikrant@seasiainfotech.com"
 }   
+
+resource "aws_cloudwatch_event_rule" "console" {
+  name        = "capture-aws-sign-in"
+  description = "Capture each AWS Console Sign In"
+
+  event_pattern = <<EOF
+{
+  "source": ["aws.ec2"]
+}
+EOF
+}
+
+resource "aws_cloudwatch_event_target" "sns" {
+  rule      = aws_cloudwatch_event_rule.console.name
+  target_id = aws_sns_topic.user_updates.name
+  arn       = aws_sns_topic.user_updates.id
+}
