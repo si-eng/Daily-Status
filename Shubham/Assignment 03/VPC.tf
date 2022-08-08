12171121766ef6087cdb6125a23178ad85472c4d
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2022-08-08 10:12:11 +0530

    Update VPC.tf

--- Shubham/Assignment 03/VPC.tf
+++ Shubham/Assignment 03/VPC.tf
@@ -1,5 +1,4 @@
-# creating VPC
-resource "aws_vpc" "terraform" {
+resource "aws_" "shubvpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support = true
@@ -11,70 +10,54 @@ resource "aws_vpc" "terraform" {
   }
 }
 
-# creating subnet
-# resource "aws_subnet" "subnet-1" {
-#   vpc_id = aws_vpc.terraform.id
-#   cidr_block = "10.0.2.0/24"
-#   availability_zone = "ap-northeast-1a"
-#   tags = {
-#     "Name" = "Tarreform"
-#   }
-# }
-resource "aws_subnet" "subnet-2" {
-  vpc_id = aws_vpc.terraform.id
+resource "aws_subnet" "mysubnet1" {
+  vpc_id = aws_vpc.shubham.id
   cidr_block = "10.0.4.0/24"
   availability_zone = "us-east-1f"
   tags = {
-    "Name" = "Tarreform"
+    "Name" = "mysubnet"
   }
 }
-resource "aws_subnet" "subnet-3" {
-  vpc_id = aws_vpc.terraform.id
+resource "aws_subnet" "mysubnet2" {
+  vpc_id = aws_vpc.shubham.id
   cidr_block = "10.0.5.0/24"
   availability_zone = "us-east-1d"
   tags = {
-    "Name" = "Tarreform"
+    "Name" = "mysubnet"
   }
 }
 
-# creating internet gate way
 resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.terraform.id
+  vpc_id = aws_vpc.shubham.id
   tags = {
-    "Name" = "Terraform"
+    "Name" = "mygateway"
   }
 }
 
-# crating security group
 resource "aws_security_group" "allow" {
   name        = "allow"
- # description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.terraform.id
+  vpc_id      = aws_vpc.shubham.id
 
   ingress {
-    # description      = "TLS from VPC"
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    #ipv6_cidr_blocks = [vpc-0e058d6c1c81f6a27]
+    
   }
   ingress {
-    # description      = "TLS from VPC"
     from_port        = 5050
     to_port          = 5050
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    #ipv6_cidr_blocks = [vpc-0e058d6c1c81f6a27]
+
   }
 
   ingress {
-    # description      = "TLS from VPC"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    #ipv6_cidr_blocks = [vpc-0e058d6c1c81f6a27]
   }
 
   egress {
@@ -91,8 +74,8 @@ resource "aws_security_group" "allow" {
 }
 
 
-resource "aws_route_table" "terraform" {
-  vpc_id = aws_vpc.terraform.id
+resource "aws_route_table" "shubham" {
+  vpc_id = aws_vpc.shubham.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -105,11 +88,11 @@ resource "aws_route_table" "terraform" {
   }
 
   tags = {
-    Name = "terraform"
+    Name = "shubham"
   }
 }
-# 5. Associate subnet with Route Table
+
 resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.subnet-2.id
-  route_table_id = aws_route_table.terraform.id
+  subnet_id      = aws_subnet.mysubnet1.id
+  route_table_id = aws_route_table.shubham.id
 }
