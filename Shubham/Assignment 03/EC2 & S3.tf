b22dbfe88a5883ef6f64d4d277b422f9ec48573c
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2022-08-18 10:17:54 +0530

    Update EC2 & S3.tf

--- Shubham/Assignment 03/EC2 & S3.tf
+++ Shubham/Assignment 03/EC2 & S3.tf
@@ -5,32 +5,28 @@ resource "aws_instance" "host" {
   associate_public_ip_address = true
   subnet_id = aws_subnet.mysubnet1.id
   vpc_security_group_ids = [aws_security_group.allow.id]
-  iam_instance_profile =  "${aws_iam_instance_profile.ec2_profile.name}"
 
   tags = {
-    Name = "Deployment"
+    Name = "Shubham"
   }
+
   user_data = <<-EOF
                 #!/bin/bash
-                sudo apt update -y
-                
-                sudo apt install ruby-full -y
-                sudo apt install wget -y 
-                sudo wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
-                sudo chmod +x ./install
-                sudo ./install auto  
-                sudo service codedeploy-agent status 
-                sudo service codedeploy-agent restart
+
+                sudo apt update  
+                sudo apt install nginx -y
+                sudo systemctl start nginx
+                                
               EOF
 }
 
 resource "aws_s3_bucket" "bucket" {
-  bucket = "<BUCKET NAME>"  
+  acl    = "public-read"
+  bucket = "jhgffdcvbyhn876ghjbh"  
   tags = {
-    Name        = "s3bucket"
+    Name        = "My s3 bucket"
   }  
 }
-
 resource "aws_s3_bucket_acl" "b_acl" {
   bucket = aws_s3_bucket.bucket.id
   acl    = "private"
