9db7ea50103f3db28387c7422d5b4a2c4d675584
gpswarwal <99944302+gpswarwal@users.noreply.github.com>
2022-11-03 22:42:34 +0530

    Delete file9.py

--- Gurpreet/file9.py
+++
@@ -1,5 +0,0 @@
-# 9. Write a Python program to display the examination schedule. (extract the date from exam_st_date).
-# exam_st_date = (11, 12, 2014)
-
-exam_date = (11,12,2014)
-print("Exam date starts from: %i/%i/%i"%exam_date)
