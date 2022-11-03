a5c7c3fa3994db42255711eed5fb3dbfe635f1ab
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2022-11-03 10:40:34 +0530

    Add files via upload

---
+++ Shubham/Web Application Deployment using Terraform/EC2 and S3 Bucket.tf
@@ -0,0 +1,23 @@
+resource "aws_s3_bucket" "bucket" {
+  acl    = "public-read"
+  bucket = "jhgffdcvbyhn876ghjbh"  
+  tags = {
+    Name        = "My s3 bucket"
+  }  
+}
+resource "aws_s3_bucket_acl" "b_acl" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_website_configuration" "staging" {
+  bucket = aws_s3_bucket.bucket.bucket
+
+   index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "/index.html"
+  }
+}
