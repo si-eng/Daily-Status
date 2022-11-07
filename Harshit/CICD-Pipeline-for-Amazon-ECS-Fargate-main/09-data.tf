727b2b377d88cf55872392d87e3099812d7932d2
harshhhit <100746928+harshhhit@users.noreply.github.com>
2022-11-07 13:19:40 +0530

    Update 09-data.tf

--- Harshit/CICD-Pipeline-for-Amazon-ECS-Fargate-main/09-data.tf
+++ Harshit/CICD-Pipeline-for-Amazon-ECS-Fargate-main/09-data.tf
@@ -1,5 +1,5 @@
 data "aws_iam_role" "pipeline_role" {
-  name = "AWSCodePipelineServiceRole-ap-south-1-django"
+  name = "AWSCodePipelineServiceRole-us-east-1-django"
 }
 
 data "aws_iam_role" "ecs-task" {
