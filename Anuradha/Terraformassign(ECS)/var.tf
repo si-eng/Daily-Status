27279ff5fa2f55c4f887a7f87e39f6c7605a3cba
Siarajput421 <100112295+Siarajput421@users.noreply.github.com>
2022-11-09 21:08:27 +0530

    Delete var.tf

--- Anuradha/Terraformassign(ECS)/var.tf
+++
@@ -1,9 +0,0 @@
-variable "artifacts_bucket_name_aacclll" {
-  description = "S3 Bucket for storing artifacts"
-  default     = "ecsbucketerraformaclll"
-}
-
-variable "env" {
-  description = "Depolyment environment"
-  default     = "dev"
-}
