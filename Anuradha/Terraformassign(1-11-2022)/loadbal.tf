b8ebea7345bcbe7021c5a1b60b59e1dbac2b9e66
Siarajput421 <100112295+Siarajput421@users.noreply.github.com>
2022-11-09 10:50:38 +0530

    Add files via upload

--- Anuradha/Terraformassign(1-11-2022)/loadbal.tf
+++ Anuradha/Terraformassign(1-11-2022)/loadbal.tf
@@ -1,49 +1,28 @@
-#creating application load balancer
-resource "aws_lb" "test-lb" {
-    name = "test-lb"
-    load_balancer_type = "application"
-    internal = false
-    security_groups = [aws_security_group.public.id]
-    subnets = [aws_subnet.terrapublicpublic.id,aws_subnet.terraprivate.id]
-}
-
-
-
-#creating target group
-resource "aws_lb_target_group" "test-tg" {
-    name = "test-tg"
-    port = 80
-    protocol = "HTTP"
-    vpc_id = aws_vpc.terraform-vpc.id
-}
-
-
-#creating listener group
-
-resource "aws_lb_listener" "test-listener" {
-    load_balancer_arn = aws_lb.test-lb.arn
-    port = 80
-    protocol = "HTTP"
-
-    default_action {
-      type = "forward"
-      target_group_arn = aws_lb_target_group.test-tg.arn
-    }
+resource "aws_lb" "app-lb" {
+  name               = "app-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.public.id]
+  subnets            = ["${aws_subnet.terrapublicpublic.id}", "${aws_subnet.terraprivate.id}"]
 
 }
 
-
-#target group attachment with ec2 instances
-resource "aws_lb_target_group_attachment" "tg-1" {
-    target_group_arn = aws_lb_target_group.test-tg.arn
-    target_id = aws_instance.anuuuuuuuuuu-inst1.id
-    port = 80
+resource "aws_lb_target_group" "tg-group" {
+  name        = "tg-group"
+  port        = "8000"
+  protocol    = "HTTP"
+  vpc_id      = "${aws_vpc.terraform-vpc.id}"
+  target_type = "ip"
 
 }
 
-resource "aws_lb_target_group_attachment" "tg-2" {
-    target_group_arn = aws_lb_target_group.test-tg.arn
-    target_id = aws_instance.anuuuuuuuuuu-inst2.id
-    port = 80
+resource "aws_lb_listener" "lb-listener" {
+  load_balancer_arn = "${aws_lb.app-lb.arn}"
+  port              = "80"
+  protocol          = "HTTP"
 
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.tg-group.arn}"
+  }
 }
---
+++ Anuradha/Terraformassign(1-11-2022)/pipeline.tf
@@ -0,0 +1,114 @@
+resource "aws_iam_role" "codepipeline_role" {
+  name = "ecspiperole1122"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codepipeline.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "codepipeline_policy" {
+  name = "ecspolicytest"
+  role = aws_iam_role.codepipeline_role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect":"Allow",
+      "Action": [
+        "*"
+      ],
+      "Resource": [
+        "*" 
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "*"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+
+
+resource "aws_codepipeline" "ecs_pipeline" {
+  name     = "ecs_pipeline"
+  role_arn = aws_iam_role.codepipeline_role.arn
+
+  artifact_store {
+    location = var.artifacts_bucket_name_aacclll
+    type     = "S3"
+  }
+  # SOURCE
+  stage {
+    name = "Source"
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub v2"
+      version          = "1"
+      input_artifacts  = []
+      output_artifacts = ["source_output"]
+
+      configuration = {
+        Owner      = "Siarajput421"
+        Repo       = "nodejs"
+        Branch     = "main"
+        OAuthToken = "********************"
+      }
+    }
+  }
+  # BUILD
+  stage {
+    name = "Build"
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      version          = "1"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+
+      configuration = {
+        ProjectName = "ecs_elasticcloud_build"
+      }
+    }
+  }
+  # DEPLOY
+  stage {
+    name = "Deploy"
+    action {
+      name            = "Deploy"
+      category        = "Deploy"
+      owner           = "AWS"
+      provider        = "ECS"
+      version         = "1"
+      input_artifacts = ["build_output"]
+
+      configuration = {
+        ClusterName = "clusterDev"
+        ServiceName = "react-Service"
+        FileName    = "imagedefinitions.json"
+      }
+    }
+  }
+}
