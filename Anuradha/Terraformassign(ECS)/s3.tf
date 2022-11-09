501c44c9719ffcaaa7addf9e24f2bbfa19e79908
Siarajput421 <100112295+Siarajput421@users.noreply.github.com>
2022-11-09 21:07:48 +0530

    Delete s3.tf

--- Anuradha/Terraformassign(ECS)/s3.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "artifacts_bucket_name_aacclll" {
-  bucket = "ecsbucketerraformaclll"  
-  #acl    = "private"
-
-  tags = {
-    Name        = "ecsterraformacll"
-  }  
-}
-  
