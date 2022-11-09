4974fc95b9ca319e9bc3a5f82be6fd89a6ed752e
Siarajput421 <100112295+Siarajput421@users.noreply.github.com>
2022-11-09 21:09:12 +0530

    Add files via upload

---
+++ Anuradha/Terraformassign(ECS)/Terraformassign(ECS)/var.tf
@@ -0,0 +1,9 @@
+variable "artifacts_bucket_name_aacclll" {
+  description = "S3 Bucket for storing artifacts"
+  default     = "ecsbucketerraformaclll"
+}
+
+variable "env" {
+  description = "Depolyment environment"
+  default     = "dev"
+}
