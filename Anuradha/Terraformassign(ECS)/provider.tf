06b31c71fac1eae274a79f41f2d31d47684747b3
Siarajput421 <100112295+Siarajput421@users.noreply.github.com>
2022-11-09 21:07:37 +0530

    Delete provider.tf

--- Anuradha/Terraformassign(ECS)/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-  region  = "us-east-1"
-  #shared_credentials_file = "$HOME/.aws/credentials" # default
-  #profile = "default" # you may change to desired profile
-  #version = "~> 2.7.7"
-}
