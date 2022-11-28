547c311dc777df305f3dabf3283a0d47702cd091
Siarajput421 <100112295+Siarajput421@users.noreply.github.com>
2022-11-28 20:57:03 +0530

    Update SERVICE-2.tf

--- Anuradha/6-microservices(assignment)/terraform/SERVICE-2.tf
+++ Anuradha/6-microservices(assignment)/terraform/SERVICE-2.tf
@@ -116,7 +116,7 @@ resource "aws_codepipeline" "my_pipeline1" {
          Owner      = "Siarajput421"
         Repo       = "Dockerpipe1"
         Branch     = "main"
-        OAuthToken = "****"
+        OAuthToken = "*****"
       }
     }
   }
