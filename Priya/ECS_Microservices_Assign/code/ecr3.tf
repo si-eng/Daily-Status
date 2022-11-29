c9d879e449d6ce34b73cab5e899a5146a7143338
priyashukla1411 <100112297+priyashukla1411@users.noreply.github.com>
2022-11-29 11:32:22 +0530

    Update ecr3.tf

--- Priya/ECS_Microservices_Assign/code/ecr3.tf
+++ Priya/ECS_Microservices_Assign/code/ecr3.tf
@@ -163,3 +163,70 @@ resource "aws_codepipeline" "assignment_pipeline3" {
   }
 }
 
+#### Autooooo###############
+resource "aws_appautoscaling_policy" "scale_up_policy" {
+  name               = "scale_up_policy"
+  depends_on         = [aws_appautoscaling_target.ecs_target]
+  service_namespace  = "ecs"
+  resource_id        = "ecr3/ecs-cluster/html_service3"
+  scalable_dimension = "ecs:service:DesiredCount"
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 60
+    metric_aggregation_type = "Maximum"
+    step_adjustment {
+      metric_interval_lower_bound = 0
+      scaling_adjustment          = 1
+    }
+  }
+}
+
+resource "aws_appautoscaling_policy" "scale_down_policy" {
+  name               = "scale_down_policy"
+  depends_on         = [aws_appautoscaling_target.ecs_target]
+  service_namespace  = "ecs"
+  resource_id        = "ecr/ecs-cluster/html_service1"
+  scalable_dimension = "ecs:service:DesiredCount"
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 60
+    metric_aggregation_type = "Maximum"
+    step_adjustment {
+      metric_interval_upper_bound = 0
+      scaling_adjustment          = -1
+    }
+  }
+}
+################  watchhhhhhh#########
+resource "aws_cloudwatch_metric_alarm" "up" {
+  alarm_name          = "up"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 3                              #"The number of periods over which data is compared to the specified threshold for max cpu metric alarm"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = 60                             #"The number of periods over which data is compared to the specified threshold for min cpu metric alarm"
+  statistic           = "Maximum"
+  threshold           = 80
+  
+  alarm_actions = [aws_appautoscaling_policy.scale_up_policy.arn]
+
+  
+}
+
+
+resource "aws_cloudwatch_metric_alarm" "down" {
+  alarm_name          = "down"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = 3
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 10
+  
+  alarm_actions = [aws_appautoscaling_policy.scale_down_policy.arn]
+
+  
+}
+
+
