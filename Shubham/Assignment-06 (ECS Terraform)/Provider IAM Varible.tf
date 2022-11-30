9137e9b69f8de40edc13ebb49efb38dfaad60fd4
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2022-11-30 12:24:28 +0530

    Add files via upload

---
+++ Shubham/Assignment-06 (ECS Terraform)/Provider IAM Varible.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region  = "us-east-1"
+}
+
+########################################## IAM Role #############################################
+
+data "aws_iam_role" "ecs-task" {
+  name = "taskexcution"
+}
+
+############################################ Varible #############################################
+
+variable "env" {
+  description = "Depolyment environment"
+  default     = "dev"
+}
+
+
+
+variable "artifacts_bucket_name" {
+  description = "S3 Bucket for storing artifacts"
+  default     = "shubhamchouhantest"
+}
