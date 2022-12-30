af965db6fe466cbb1bfe9a6f87bec64d8a68d2dd
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2022-12-30 15:34:51 +0530

    Add files via upload

---
+++ Shubham/Assignment-11 (AWS Lambda Function)/Lambda function to create VPC/VPC.py
@@ -0,0 +1,52 @@
+import boto3
+
+# http://boto3.readthedocs.io/en/latest/reference/services/ec2.html#service-resource
+
+ec2 = boto3.resource('ec2', region_name='us-east-2')
+
+# create VPC
+vpc = ec2.create_vpc(CidrBlock='192.168.0.0/16')
+# we can assign a name to vpc, or any resource, by using tag
+vpc.create_tags(Tags=[{"Key": "Name", "Value": "default_vpc"}])
+vpc.wait_until_available()
+print(vpc.id)
+
+# create then attach internet gateway
+ig = ec2.create_internet_gateway()
+vpc.attach_internet_gateway(InternetGatewayId=ig.id)
+print(ig.id)
+
+# create a route table and a public route
+route_table = vpc.create_route_table()
+route = route_table.create_route(
+    DestinationCidrBlock='0.0.0.0/0',
+    GatewayId=ig.id
+)
+print(route_table.id)
+
+# create subnet
+subnet = ec2.create_subnet(CidrBlock='192.168.1.0/24', VpcId=vpc.id)
+print(subnet.id)
+
+# associate the route table with the subnet
+route_table.associate_with_subnet(SubnetId=subnet.id)
+
+# Create sec group
+sec_group = ec2.create_security_group(
+    GroupName='slice_0', Description='slice_0 sec group', VpcId=vpc.id)
+sec_group.authorize_ingress(
+    CidrIp='0.0.0.0/0',
+    IpProtocol='icmp',
+    FromPort=-1,
+    ToPort=-1
+)
+print(sec_group.id)
+
+def lambda_handler(event, context):
+    init_script = """#!/bin/bash
+                yum update -y
+                yum install -y httpd24
+                service httpd start
+                chkconfig httpd on
+                echo > /var/www/html/index.html
+                shutdown -h +5"""
